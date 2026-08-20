0f6b5c3de084b2f7cf150b4d427a0cec1ddfdd45
Daniel <akirazcs@gmail.com>
2026-08-20 11:04:31 -0300

    fix:Remove dead code

--- backend/Account/models.py
+++ backend/Account/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
@@ -15,4 +14,3 @@ class User(AbstractBaseUser):
         
     def __str__(self) -> str:
         return self.username
-    
