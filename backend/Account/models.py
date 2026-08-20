f6b9034b8641e8b9475dc662667b64b042982b4a
Daniel <akirazcs@gmail.com>
2026-08-20 11:03:13 -0300

    feat:Add the first account model

--- backend/Account/models.py
+++ backend/Account/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=50, null=False, unique=True)
+    email = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=50)
+    
+    class Meta:
+        verbose_name = 'User'
+        
+    def __str__(self) -> str:
+        return self.username
+    
