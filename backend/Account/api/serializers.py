2f1ee3c6700633feeafac56983ab736ed05fdb82
Daniel <akirazcs@gmail.com>
2026-08-21 18:57:30 -0300

    Feat:Add UserManager to Account model

---
+++ backend/Account/api/serializers.py
@@ -0,0 +1,4 @@
+from rest_framework import serializers
+from ..models import User
+from django.contrib.auth.hashers import make_password
+
--- backend/Account/models.py
+++ backend/Account/models.py
@@ -1,8 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 
 # Create your models here.
 
+class UserManager(BaseUserManager):
+    def create_user(self, username:str, email:str, password: str = None, **extra_fields) -> "User":
+        if not username:
+            raise ValueError('O campo "Username" não pode ser inválido')
+        if not email:
+            raise ValueError('O campo "Email" não pode ser inválido')
+        
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 class User(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=100)
