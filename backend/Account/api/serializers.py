51a77eb8978910b8ef79f511a76771571bb54f72
Daniel <akirazcs@gmail.com>
2026-08-25 08:46:59 -0300

    Feat:Add Register and Login Serializer

--- backend/Account/api/serializers.py
+++ backend/Account/api/serializers.py
@@ -1,4 +1,40 @@
 from rest_framework import serializers
-from ..models import User
 from django.contrib.auth.hashers import make_password
+from ..models import User
+from django.contrib.auth import authenticate # Django's built-in password checker
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.Charfield(write_only=True, min_lenght=8)
+    
+    class Meta:
+        model = User
+        fields = ('username','email','password')
+        
+    def create(self, validated_data):
+        validated_data['passoword'] = make_password(validated_data['password']) #hash the password
+        return User.objects.create(**validated_data)
+    
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        
+        try:
+            user = User.objects.get(email=email)
+            username = user.username
+        except:
+            raise serializers.ValidationError("Invalid credentials")
+        
+        user = authenticate(username=username, password=password)
+        
+        if not user:
+            raise serializers.ValidationError("invalid credentials")
+        
+        attrs['user'] = user
+        return attrs
+    
+        
+    
